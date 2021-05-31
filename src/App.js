86ee96fb503930e680760737e017ca767592a2dc
Fercho5656 <morenomoranfernando@gmail.com>
2021-05-31 09:07:57 -0500

    Created basic routing structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
+import { Footer } from './Components/Footer'
+import { Login } from './Routes/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Login />
+        </Route>
+        <Route path='/atm'>
+          {/* ATM for clients*/}
+        </Route>
+        <Route path='/dashboard'>
+          {/* Dashboard for users */}
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
