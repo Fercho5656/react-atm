b1d58b537c694452044e2ccafec94054b641b678
Fercho5656 <morenomoranfernando@gmail.com>
2021-05-31 11:01:01 -0500

    Created context to login into accounts

---
+++ src/Context/UsersContext.js
@@ -0,0 +1,19 @@
+import { createContext, useState } from 'react'
+
+export const UsersContext = createContext()
+
+export const UsersProvider = (props) => {
+    //Hardcoded user cause there's no database
+    const [users, setUsers] = useState([{
+        username: 'Fercho',
+        password: 'Fernando',
+        employeeId: 1
+    }
+    ])
+
+    return (
+        <UsersContext.Provider value={[users, setUsers]}>
+            {props.children}
+        </UsersContext.Provider>
+    )
+}
