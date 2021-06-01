9445973479b591f1dc24e0dffbd2fb2ae1a78a44
Fercho5656 <morenomoranfernando@gmail.com>
2021-06-01 12:12:16 -0500

    Added clients and ATM balance to context

--- src/Context/UsersContext.js
+++ src/Context/UsersContext.js
@@ -11,10 +11,39 @@ export const UsersProvider = (props) => {
         role: 'user'
     }])
 
-    const [clients, setClients] = useState(['hola'])
+    const [clients, setClients] = useState([{
+        username: 'ElDueño',
+        password: 'MiBanco123',
+        dischargeDate: "31/5/2021",
+        accountNumber: 420,
+        balance: 1_000_000
+    },
+    {
+        username: 'ElDueño2',
+        password: 'MiBanco123',
+        dischargeDate: "1/6/2021",
+        accountNumber: 777,
+        balance: 1_500_000
+    },
+    ])
+
+    const [atm, setAtm] = useState({
+        1000: 100,
+        500: 100,
+        200: 100,
+        100: 100,
+        50: 100
+    })
+
+    const [actualUser, setActualUser] = useState()
 
     return (
-        <UsersContext.Provider value={{ users: [users, setUsers], clients: [clients, setClients] }}>
+        <UsersContext.Provider value={{
+            users: [users, setUsers],
+            clients: [clients, setClients],
+            actualUser: [actualUser, setActualUser],
+            atm: [atm, setAtm]
+        }}>
             {props.children}
         </UsersContext.Provider>
     )
