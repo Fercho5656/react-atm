b42b3762c039462590870ff561f2ab50dde84bc5
Fercho5656 <morenomoranfernando@gmail.com>
2021-06-15 11:58:29 -0500

    Removed log

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,7 +1,5 @@
 export const userAuthenticated = (userData = {}, users = []) => {
     //Checks for user existence
-    console.log('userData: ', userData)
-    console.log('user: ', users)
     if (users.some(user => user.username === userData.username)) {
         //Checks for password coincidence
         if (users.some(user => user.password === userData.password)) {
