1d87cc1ee307c68f400ec8f4eed4646e86d83c6d
Fercho5656 <morenomoranfernando@gmail.com>
2021-06-15 11:59:09 -0500

    Added Table, Grid and styles

--- src/Routes/Dashboard.js
+++ src/Routes/Dashboard.js
@@ -1,8 +1,22 @@
-
+import { Grid } from '@material-ui/core'
 import { ClientsTable } from '../Components/ClientsTable'
+import { useStyles } from '../Styles/DashboardStyles'
 
 export const Dashboard = () => {
+
+    const classes = useStyles()
+
     return (
-        <ClientsTable />
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="flex-start"
+            className={classes.grid}
+        >
+            <Grid item lg={12}>
+                <ClientsTable />
+            </Grid>
+        </Grid>
     )
 }
